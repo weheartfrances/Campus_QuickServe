d6e8774253f319e9a5922d36fe307f6dafa7ef68
Frances <weheartfrances@gmail.com>
2026-03-11 17:02:59 +0200

    Beginning of assignment, I am excited!

---
+++ Desktop/QuickServeCampus/src/main/java/com/mycompany/quickservecampus/QuickServeCampus.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quickservecampus;
+
+/**
+ *
+ * @author frances
+ */
+public class QuickServeCampus {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+ 
+
+// Start of assignment 
