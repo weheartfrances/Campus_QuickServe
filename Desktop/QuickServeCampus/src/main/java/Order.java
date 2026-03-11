ff1f82b34e50c1cb5cb0ab972ad2767b2786a9df
Frances <weheartfrances@gmail.com>
2026-03-11 20:59:16 +0200

    Method added for subtotal calculation and discount

--- Desktop/QuickServeCampus/src/main/java/Order.java
+++ Desktop/QuickServeCampus/src/main/java/Order.java
@@ -9,18 +9,42 @@
  */
 public class Order {
     
+    //  Attributes
+    
     private String customerName;
     private String studentNumber;
     private String itemOrdered;
     private int quantity;
     private double pricePerItem;
     
+    // Constructor 
+    
     public Order(String customerName, String studentNumber, String itemOrdered, int wuantity, double pricePerItem){
         this.customerName = customerName;
         this.studentNumber = studentNumber;
         this. itemOrdered = itemOrdered;
         this.quantity = quantity;
-        this.pricePerItem = pricePerItem;
+        this.pricePerItem = pricePerItem;}
+    
+        //Method
+        // Calculating subtotal
+    
+    public double calculateSubtotal(){
+        double subtotal = quantity * pricePerItem;
+        return subtotal; }
+    
+         // Discount
+    
+    public double calculateDiscount(){
+        double subtotal = calculateSubtotal();
+        double discount = 0.0;
+        
+        // Quantity Check 
+        
+        if (quantity > 3)
+    
+    
+    
         
     
         
