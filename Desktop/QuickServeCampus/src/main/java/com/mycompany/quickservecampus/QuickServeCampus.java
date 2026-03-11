771557210f26a71d642564030fd71b4094c8bfb5
Frances <weheartfrances@gmail.com>
2026-03-11 19:08:27 +0200

    Beginning of assignment, I am excited! Order class has been created, including attributes and constructor

--- Desktop/QuickServeCampus/src/main/java/com/mycompany/quickservecampus/QuickServeCampus.java
+++ Desktop/QuickServeCampus/src/main/java/com/mycompany/quickservecampus/QuickServeCampus.java
@@ -8,12 +8,36 @@ package com.mycompany.quickservecampus;
  *
  * @author frances
  */
-public class QuickServeCampus {
+Order.java
+public class order {
+    
+    private String customerName;
+    private String studentNumber;
+    private String itemOrdered;
+    private int quantity;
+    private double pricePerItem;
+    
+    public Order(String customerName, String studentNumber, String itemOrdered, int quantity, double pricePerItem); {
+    this.customerName = customerName;
+    this.studentNumber= studentNumber;
+    this.itemOrdered = itemOrdered;
+    this.quantity = quantity;
+    this.pricePerItem = pricePerItem;
+    
+           
+        
+        
+}
+
+    
+    
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+      // Customer name
+      string name = JOptionPane.showinputDialog("What is your name?");
+      
+      // Student number
+      string studentNumber = JOptionPane.showInputDialog("Please enter your student number: ")
     }
-}
- 
-
-// Start of assignment 
+} 
+         
