89cd87df4ce9595a49ae2c521fc35d11a16480f6
Frances <weheartfrances@gmail.com>
2026-03-11 20:38:04 +0200

    Beginning of assignment, I am excited! Order class has been created, including attributes and constructor

---
+++ Desktop/QuickServeCampus/src/main/java/Order.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author frances
+ */
+public class Order {
+    
+    private String customerName;
+    private String studentNumber;
+    private String itemOrdered;
+    private int quantity;
+    private double pricePerItem;
+    
+    public Order(String customerName, String studentNumber, String itemOrdered, int wuantity, double pricePerItem){
+        this.customerName = customerName;
+        this.studentNumber = studentNumber;
+        this. itemOrdered = itemOrdered;
+        this.quantity = quantity;
+        this.pricePerItem = pricePerItem;
+        
+    
+        
+    }
+            
+    
+}
